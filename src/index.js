84ef6920ad1932fd1bcad35200fa7b4c04322457
haeyun1 <jhy041020@handong.ac.kr>
2024-11-21 17:14:36 +0900

    change index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import App from "./App"; // App 컴포넌트 경로
+import App from "./components/App"; // App 컴포넌트 경로
 import "bootstrap/dist/css/bootstrap.min.css"; // Bootstrap 스타일
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
